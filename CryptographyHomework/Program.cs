d4b0c8f179180afd4547c30141db57d9d75351ee
Barcsai Dominik Zoltán <barcsai.dominik@gmail.com>
2025-03-23 16:33:34 +0100

    Add project files.

---
+++ CryptographyHomework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CryptographyHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
